ceb8a2609cff78d0a9fcdb58b84ff60ff148d6c5
Ivan Karagyozov <ivankaragyozov0@gmail.com>
2023-01-01 21:55:33 +0200

    Added pagination

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,20 +1,29 @@
 import { getAllOffers } from '../api/offers.js';
 import { html } from '../library.js';
 
-const catalogTemplate = (offers) => html`
+const catalogTemplate = (offers, page, pages) => html`
     <section id="dashboard">
         <h2>Supplies</h2>
         <ul class="offer-wrapper">
             <!-- Display products if there are any -->
             ${offers.length === 0
-            ? html`<h2>There are no products yet.</h2>`
-            : offers.map(offerCard)}
+                    ? html`<h2>There are no products yet.</h2>`
+                    : offers.map(offerCard)}
         </ul>
+        <div class="pagination">
+            ${page > 1
+                    ? html`<a class="prev-page" href="javascript:void(0)" onclick="prevPage()">Prev</a>`
+                    : ''}
+            <span class="page-info">Page ${page} of ${pages}</span>
+            ${page < pages
+                    ? html`<a class="next-page" href="javascript:void(0)" onclick="nextPage()">Next</a>`
+                    : ''}
+        </div>
     </section>
 `;
 
 const offerCard = (offer) => html`
-    <li class="offer">
+    <li class="offer" aria-live="polite">
         <img src="${offer.imageUrl}" alt="example1"/>
         <p>
             <strong><span class="title">${offer.name}</span></strong>
@@ -24,8 +33,59 @@ const offerCard = (offer) => html`
     </li>
 `;
 
-
 export async function catalogView(ctx) {
+    const offersPerPage = 3;
+    let page = 1;
     const offers = await getAllOffers();
-    ctx.render(catalogTemplate(offers));
+    const pages = Math.ceil(offers.length / offersPerPage);
+
+    function showPage(page) {
+        const startIndex = (page - 1) * offersPerPage;
+        const endIndex = startIndex + offersPerPage;
+
+        const pageOffers = offers.slice(startIndex, endIndex);
+
+        ctx.render(catalogTemplate(pageOffers, page, pages));
+    }
+
+
+    window.prevPage = function() {
+        if (page > 1) {
+            page--;
+            showPage(page);
+        }
+    }
+
+    window.nextPage = function() {
+        if (page < pages) {
+            page++;
+            showPage(page);
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const paginationContainer = document.querySelector('.pagination');
+
+        // Attach event listeners to the pagination links
+        paginationContainer.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            // Get the element that was clicked
+            const clickedElement = event.target;
+
+            // Check if the element has the prev-page class
+            if (clickedElement.classList.contains('prev-page')) {
+                prevPage();
+            }
+            // Check if the element has the next-page class
+            if (clickedElement.classList.contains('next-page')) {
+                nextPage();
+            }
+
+
+        });
+    });
+
+// Show the first page
+    showPage(page);
 }
